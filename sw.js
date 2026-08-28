d5475eba73ffcd90b2e7d225d890fcda0efff7a9
Lohan Sampaio <73318105+Lohansampaio@users.noreply.github.com>
2026-08-28 15:03:30 -0300

    Refresh PWA cache for event mode

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='walkie-love-v2';
+const CACHE='walkie-love-v3';
 const APP=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
